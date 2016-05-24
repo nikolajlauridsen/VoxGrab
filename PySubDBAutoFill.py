304f25f1c3f5b81d9ae138288b90016c1a9a3d56
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-05-24 18:12:50 +0200

    Adding error handling

--- PySubDBAutoFill.py
+++ PySubDBAutoFill.py
@@ -20,13 +20,21 @@ user_agent = "SubDB/1.0 (PySubDBAutoFill/0.1; http://github.com/nikolajlauridsen
 format = input("file format: ")
 files = glob.glob('*.' + format)
 headers = {'User-Agent': user_agent}
+fail_count = 0
 
 for file in files:
     f_hash = get_hash(file)
     payload = {'action': 'download', 'hash': f_hash, 'language': 'en'}
     print("Getting subtitle for " + file)
     response = requests.get(base_url, headers=headers, params=payload)
-    f = open(file + '.srt', 'wb')
-    f.write(response.content)
-    f.close
-    print("done")
+    if response.status_code == 200:
+        f = open(file + '.srt', 'wb')
+        f.write(response.content)
+        f.close
+        print("done")
+    else:
+        print("There's unfortunately no subtitle for this file at the moment")
+        fail_count += 1
+
+    if fail_count > 8:
+        break
