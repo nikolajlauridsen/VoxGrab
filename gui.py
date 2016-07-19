94389146d05ede8dce4fb8f793438e9c6e10e573
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-07-19 23:00:14 +0200

    Making progress

--- gui.py
+++ gui.py
@@ -2,23 +2,39 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 
+import os
+
 
 class SubDownload(Frame):
 
     def print_dir(self):
         print(self.directory)
 
+    def print_files(self):
+        self.get_files()
+        for file in self.files:
+            print(file)
+
     def get_directory(self):
         self.directory = filedialog.askdirectory()
 
-    def createWidgets(self):
-        self.print_button = ttk.Button(root, text="Print dir", command=self.print_dir).grid(column=1, row=1, sticky=S)
-        self.choose_dir = ttk.Button(root, text="Choose folder", command=self.get_directory).grid(column=2, row=1, sticky=S)
+    def get_files(self):
+        self.files = os.listdir(path=self.directory)
+
+    def create_widgets(self):
+        self.title_label = ttk.Label(text='Subtitle downloader').grid(column=2, row=1, sticky=NSEW)
+        self.print_button = ttk.Button(root, text='Print dir',
+                                       command=self.print_dir).grid(column=2, row=2, sticky=S)
+        self.choose_dir = ttk.Button(root, text='Choose folder',
+                                     command=self.get_directory).grid(column=1, row=2, sticky=S)
+        self.print_files = ttk.Button(root, text='Print files',
+                                      command=self.print_files).grid(column=3, row=2, sticky=S)
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.directory = "Please choose a directory"
-        self.createWidgets()
+        self.directory = 'Please choose a directory'
+        self.files = []
+        self.create_widgets()
 
 root = Tk()
 root.title('Subtitle downloader')
